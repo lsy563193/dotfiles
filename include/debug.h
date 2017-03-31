e286f8ca2ca335964783a72294614837a00638df
alvin <1937321986@qq.com>
2017-03-31 18:28:16 +0800

    In this commit, when the robot run the wall follow mode, it will end in the pose that the robot first time hit the wall and stop in the start pose.

--- include/debug.h
+++ include/debug.h
@@ -9,6 +9,8 @@
 void debug_map(uint8_t id, int16_t endx, int16_t endy);
 #endif
 
+void debug_WF_map(uint8_t id, int16_t endx, int16_t endy);
+
 #ifdef  DEBUG_SM_MAP
 void debug_sm_map(uint8_t id, int16_t endx, int16_t endy);
 #endif
