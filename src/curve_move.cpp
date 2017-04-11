33f643ffb2334fafd756385a3dc45bebc55c799b
Patrick Chau <patrickchau@china-ilife.com>
2017-04-11 10:03:18 +0800

    curve_move.cpp: try to adjust the wheel speed use the ceil() function, so that the radius for curve movement is slightly greater from calculation.

--- src/curve_move.cpp
+++ src/curve_move.cpp
@@ -74,7 +74,7 @@ MapTouringType CurveMove_MoveToPoint()
 	}
 	ROS_WARN("%s %d: current position: (%d, %d)\tradius: %f", __FUNCTION__, __LINE__, Map_GetXPos(), Map_GetYPos(), radius);
 
-	speed = PATH_PLANNER_MAX_SPEED * (((double)radius) / CELL_COUNT_MUL_TO_CELL_SIZE - WHEEL_BASE_HALF) / (((double)radius) / CELL_COUNT_MUL_TO_CELL_SIZE + WHEEL_BASE_HALF);
+	speed = (uint8_t) ceil(PATH_PLANNER_MAX_SPEED * (((double)radius) / CELL_COUNT_MUL_TO_CELL_SIZE - WHEEL_BASE_HALF) / (((double)radius) / CELL_COUNT_MUL_TO_CELL_SIZE + WHEEL_BASE_HALF));
 	if (points[1].X == points[2].X) {
 		if (points[0].X > points[1].X) {
 			points[1].Y < points[2].Y ? speed_right = speed : speed_left = speed; 
