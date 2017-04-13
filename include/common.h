d4762393b87e98ed3f14f252c9243892a0a11bd9
Patrick Chau <patrickchau@china-ilife.com>
2017-04-13 10:14:33 +0800

    Remove common.cpp & common.h.
    They are no longer needed.

--- include/common.h
+++
@@ -1,9 +0,0 @@
-#ifndef __COMMON_H__
-#define __COMMON_H__
-
-#define	PROGNAME	"ilife"
-
-void ilife_error(const char *fmt, ...);
-void datetime(char *dt, int milli);
-int map(int val,int mmin,int mmax,int amin,int amax);
-#endif
