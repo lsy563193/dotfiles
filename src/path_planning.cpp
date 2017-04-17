c9122f48d32022a7e2384f746760ad9f7266b699
alvin <1937321986@qq.com>
2017-04-17 17:28:30 +0800

    Add the isolate checking for wall follow mode, now in wall follow mode, before it finish,it will check if in the isoalte island, it use a star to find a way to a pose out side the grid map, if it can find, then it proves that the robot is in the isolate island, if it was trapped, it proves that the robot is not in the isolate island and it can finish wall follow mode. By the way, it also improve the way to mark the obstacle when turning to make the map is closed loop.

--- src/path_planning.cpp
+++ src/path_planning.cpp
@@ -1344,6 +1344,103 @@ void path_update_cells()
 	}
 }
 
+
+/*
+ * Check whether the robot is trapped or not. The robot is trapped if there
+ * is no path to (0, 0) or home.
+ *
+ * @param
+ *
+ * @return	0 if the robot is trapped
+ * 		1 if the robot is not trapped.
+ */
+int16_t WF_path_escape_trapped() {
+
+	int16_t	val = 0;
+	uint16_t i = 0;
+	int16_t remote_x = 0, remote_y = 0;
+	int16_t x_min, x_max, y_min, y_max;
+	Point32_t	Remote_Point;
+	Point16_t   tmpPnt, pnt16ArTmp[3];
+	Remote_Point.X = 0;
+	Remote_Point.Y = 0;
+	//tmpPnt.X = countToCell(Remote_Point.X);
+	//tmpPnt.Y = countToCell(Remote_Point.Y);
+	path_get_range(&x_min, &x_max, &y_min, &y_max);
+	tmpPnt.X = x_max + 1;
+	tmpPnt.Y = y_max + 1;
+	printf("tmpPnt.X = %d, tmpPnt.Y = %d\n",tmpPnt.X, tmpPnt.Y);
+	pnt16ArTmp[0] = tmpPnt;
+
+	path_escape_set_trapped_cell(pnt16ArTmp, 1);
+
+	if ( trappedCell[0].X != remote_x || trappedCell[0].Y != remote_y ){
+		for ( i = 0; i < trappedCellSize; ++i ) {
+			printf("%s %d Check %d trapped reference cell: x: %d, y: %d\n", __FUNCTION__, __LINE__,
+			         i, trappedCell[i].X, trappedCell[i].Y);
+			/*if (is_block_accessible(trappedCell[i].X, trappedCell[i].Y) == 0) {
+				Map_Set_Cells(ROBOT_SIZE, trappedCell[i].X, trappedCell[i].Y, CLEANED);
+			}*/
+
+			//val = WF_path_find_shortest_path( positions[0].x, positions[0].y, trappedCell[i].X, trappedCell[i].Y, 0);
+			val = WF_path_find_shortest_path( positions[0].x, positions[0].y, trappedCell[i].X, trappedCell[i].Y, 0);
+			printf("%s %d: val %d\n", __FUNCTION__, __LINE__, val);
+			printf("SCHAR_MAX = %d\n", SCHAR_MAX);
+			if (val < 0 || val == SCHAR_MAX) {
+				/* No path to home, which is set when path planning is initialized. */
+				val = 0;//not isolated
+			} else {
+				val = 1;//isolated
+				break;
+			}
+		}
+	} else {
+		if (is_block_accessible(0, 0) == 1) {
+			val = WF_path_find_shortest_path(positions[0].x, positions[0].y, 0, 0, 0);
+#if DEBUG_SM_MAP
+			debug_map(SPMAP, 0, 0);
+#endif
+			printf("%s %d: pos (%d, %d)\tval: %d\n", __FUNCTION__, __LINE__, positions[0].x, positions[0].y, val);
+			if (val < 0 || val == SCHAR_MAX) {
+				/* Robot start position is blocked. */
+				val = WF_path_find_shortest_path(positions[0].x, positions[0].y, remote_x, remote_y, 0);
+				printf("%s %d: val %d\n", __FUNCTION__, __LINE__, val);
+
+#if DEBUG_MAP
+				debug_map(MAP, remote_x, remote_y);
+#endif
+
+				if (val < 0 || val == SCHAR_MAX) {
+					val = 0;
+				} else {
+					val = 1;
+				};
+			} else {
+				val = 1;
+			}
+		} else {
+			val = WF_path_find_shortest_path(positions[0].x, positions[0].y, remote_x, remote_y, 0);
+			printf("%s %d: val %d\n", __FUNCTION__, __LINE__, val);
+
+#if DEBUG_SM_MAP
+			debug_map(SPMAP, 0, 0);
+#endif
+#if DEBUG_MAP
+			debug_map(MAP, remote_x, remote_y);
+#endif
+
+			if (val < 0 || val == SCHAR_MAX) {
+				/* No path to home, which is set when path planning is initialized. */
+				val = 0;
+			} else {
+				val = 1;
+			}
+		}
+	}
+
+	printf("%s %d: val %d\n", __FUNCTION__, __LINE__, val);
+	return val;
+}
 /*
  * Check whether the robot is trapped or not. The robot is trapped if there
  * is no path to (0, 0) or home.
