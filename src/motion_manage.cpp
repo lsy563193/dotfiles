lsy563193/dotfiles

commit b169a0a97ca17f5148d7e7d0ee5d52e3f2a1ffa0

fix(core_move,motion_manage,spot): code error

src/motion_manage.cpp
@@ -226,7 +226,7 @@ MotionManage::~MotionManage()
 #else
         ROS_WARN("%s %d: Clean key pressed. Finish cleaning.", __FUNCTION__, __LINE__);
 #endif
-    else
+    else{
         ROS_WARN("%s %d: Pause cleanning.", __FUNCTION__, __LINE__);
         g_saved_work_time += get_work_time();
         ROS_WARN("%s %d: Cleaning time: %d(s)", __FUNCTION__, __LINE__, g_saved_work_time);

src/core_move.cpp
@@ -1315,7 +1315,6 @@ uint8_t cm_touring(void)
 	int cm_clean_ret = cm_cleaning();
 	if (cm_clean_ret == 0)
 		cm_go_home();
-    }
 	cm_unregister_events();
 	return 0;
 }
@@ -1595,7 +1594,7 @@ void cm_register_events()
 	/* Remote */
 	event_manager_register_and_enable_x(remote_clean, EVT_REMOTE_CLEAN, true);
 	event_manager_register_and_enable_x(remote_home, EVT_REMOTE_HOME, true);
-	event_manager_register_and_enable_x(remote_wallfollow,EVT_REMOTE_MODE_WALL_FOLLOW, true);
+	event_manager_register_and_enable_x(remote_wallfollow,EVT_REMOTE_WALL_FOLLOW, true);
 	event_manager_register_and_enable_x(remote_spot, EVT_REMOTE_SPOT, true);
 	event_manager_register_and_enable_x(remote_max, EVT_REMOTE_MAX, true);
 
@@ -2338,15 +2337,15 @@ void cm_handle_remote_spot(bool state_now, bool state_last)
 	stop_brifly();
 	reset_rcon_remote();
 	g_remote_spot = true;
-    Set_Clean_Mode(Clean_Mode_Spot);
+    set_clean_mode(Clean_Mode_Spot);
 }
 
 void cm_handle_remote_wallfollow(bool state_now,bool state_last)
 {
 	ROS_WARN("%s,%d: is called.",__FUNCTION__,__LINE__);
 	beep_for_command(true);
-	Stop_Brifly();
-	Reset_Rcon_Remote();
+	stop_brifly();
+	reset_rcon_remote();
 	g_remote_wallfollow = true;
 }
 
@@ -2392,7 +2391,7 @@ void cm_handle_battery_low(bool state_now, bool state_last)
 
 	if (g_battery_low_cnt++ > 50) {
 		t_vol = get_battery_voltage();
-		ROS_WARN("%s %d: low battery, battery < %umv is detected.", __FUNCTION__, __LINE__,t_val);
+		ROS_WARN("%s %d: low battery, battery < %umv is detected.", __FUNCTION__, __LINE__,t_vol);
 
 		if (g_go_home) {
 			v_pwr = Home_Vac_Power / t_vol;

src/spot.cpp
@@ -261,7 +261,7 @@ void spot_with_target(SpotType st,float spot_diameter)
 				spiral_type = SPIRAL_LEFT_IN;
 			}
 		}//ending while(ros::ok)
-        event_manager_enable_handler(EVT_REMOTE_MODE_SPOT,true);//enable remote spot handle
+        event_manager_enable_handler(EVT_REMOTE_SPOT,true);//enable remote spot handle
 	}//ending else if(st == CLEAN_SPOT...)
 }
 

commit af7d6ed6527269591c64c588931381fe7809486b

fix: Logic error caused by commit b169a0a97ca17f51.

src/motion_manage.cpp
@@ -213,23 +213,23 @@ MotionManage::~MotionManage()
 		s_laser = nullptr;
 	}
 
-	if (robot::instance()->isManualPaused()){
-        set_clean_mode(Clean_Mode_Userinterface);
-        wav_play(WAV_PAUSE_CLEANING);
-    }
-    robot::instance()->savedOffsetAngle(robot::instance()->getAngle());
-    ROS_WARN("%s %d: Save the gyro angle(%f) before pause.", __FUNCTION__, __LINE__, robot::instance()->getAngle());
-    extern bool g_go_home;
-    if (g_go_home)
+	if (robot::instance()->isManualPaused())
+	{
+		set_clean_mode(Clean_Mode_Userinterface);
+		wav_play(WAV_PAUSE_CLEANING);
+		robot::instance()->savedOffsetAngle(robot::instance()->getAngle());
+		ROS_WARN("%s %d: Save the gyro angle(%f) before pause.", __FUNCTION__, __LINE__, robot::instance()->getAngle());
+		extern bool g_go_home;
+		if (g_go_home)
 #if MANUAL_PAUSE_CLEANING
-        ROS_WARN("%s %d: Pause going home, g_home_point list size: %u.", __FUNCTION__, __LINE__, (uint)g_home_point.size());
+			ROS_WARN("%s %d: Pause going home, g_home_point list size: %u.", __FUNCTION__, __LINE__, (uint)g_home_point.size());
 #else
-        ROS_WARN("%s %d: Clean key pressed. Finish cleaning.", __FUNCTION__, __LINE__);
+			ROS_WARN("%s %d: Clean key pressed. Finish cleaning.", __FUNCTION__, __LINE__);
 #endif
-    else{
-        ROS_WARN("%s %d: Pause cleanning.", __FUNCTION__, __LINE__);
-        g_saved_work_time += get_work_time();
-        ROS_WARN("%s %d: Cleaning time: %d(s)", __FUNCTION__, __LINE__, g_saved_work_time);
+		else
+		ROS_WARN("%s %d: Pause cleanning.", __FUNCTION__, __LINE__);
+		g_saved_work_time += get_work_time();
+		ROS_WARN("%s %d: Cleaning time: %d(s)", __FUNCTION__, __LINE__, g_saved_work_time);
 		return;
 	}
 	if (robot::instance()->isLowBatPaused())
@@ -241,11 +241,11 @@ MotionManage::~MotionManage()
 		ROS_WARN("%s %d: Cleaning time: %d(s)", __FUNCTION__, __LINE__, g_saved_work_time);
 		return;
 	}
-    if (s_slam != nullptr)
-    {
-        delete s_slam;
-        s_slam = nullptr;
-    }
+if (s_slam != nullptr)
+	{
+		delete s_slam;
+		s_slam = nullptr;
+	}
 
 	robot::instance()->savedOffsetAngle(0);
 