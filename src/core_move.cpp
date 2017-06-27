ad90ca00170a4d2b3cb8bb732331de0517ad4e10
lsy563193 <lsy563193>
2017-06-27 13:52:11 +0800

    fix: #238 move_back and forward in follow wall

--- src/core_move.cpp
+++ src/core_move.cpp
@@ -616,7 +616,7 @@ uint16_t rounding_straight_distance()
 
 void cm_move_back(void)
 {
-	if (g_cm_move_type == CM_FOLLOW_LEFT_WALL || g_cm_move_type == CM_FOLLOW_LEFT_WALL)
+	if (g_cm_move_type == CM_FOLLOW_LEFT_WALL || g_cm_move_type == CM_FOLLOW_RIGHT_WALL)
 	{
 		g_rounding_turn_angle = round_turn_angle();
 		if (get_bumper_status() != 0)
@@ -965,28 +965,29 @@ bool is_follow_wall(Point32_t *next_point, Point32_t target_point, uint16_t dir)
 		return false;
 	}
 	g_cm_move_type = CM_LINEARMOVE;
-	ROS_ERROR("curr(%d,%d),next(%d,%d),target(%d,%d)",map_get_x_cell(), map_get_y_cell(),
-						                                        count_to_cell(next_point->X), count_to_cell(next_point->Y),
-																										count_to_cell(target_point.X), count_to_cell(target_point.Y));
-	ROS_ERROR("curr_point_y(%d),next_point_y(%d),dir(%d),g_should_follow_wall(%d)",map_get_y_count(), next_point->Y, dir, g_should_follow_wall);
+//	ROS_ERROR("curr(%d,%d),next(%d,%d),target(%d,%d)",map_get_x_cell(), map_get_y_cell(),
+//						                                        count_to_cell(next_point->X), count_to_cell(next_point->Y),
+//																										count_to_cell(target_point.X), count_to_cell(target_point.Y));
+//	ROS_ERROR("curr_point_y(%d),next_point_y(%d),dir(%d),g_should_follow_wall(%d)",map_get_y_count(), next_point->Y, dir, g_should_follow_wall);
 	if (!IS_X_AXIS(dir) || g_should_follow_wall == 0 || next_point->Y == map_get_y_count()) {
 		return false;
 	}
 
 	auto delta_y = count_to_cell(next_point->Y) - map_get_y_cell();
-	ROS_ERROR("curr_y(%d),next_y(%d),delta_y(%d),dir(%d)",map_get_y_cell(), count_to_cell(next_point->Y), delta_y, dir);
+//	ROS_ERROR("curr_y(%d),next_y(%d),delta_y(%d),dir(%d)",map_get_y_cell(), count_to_cell(next_point->Y), delta_y, dir);
 
 	if ( delta_y != 0 && std::abs(delta_y <= 2) ) {
 		g_cm_move_type = (dir == POS_X) ^ (delta_y > 0) ? CM_FOLLOW_LEFT_WALL: CM_FOLLOW_RIGHT_WALL;
-		ROS_ERROR("follow wall to new line, 2_left_3_right(%d)",g_cm_move_type);
+		ROS_INFO("follow wall to new line, 2_left_3_right(%d)",g_cm_move_type);
 	} else if(delta_y == 0){
-		ROS_ERROR("don't need to go to new line. curr_x(%d)", count_to_cell(next_point->X));
+//		ROS_ERROR("don't need to go to new line. curr_x(%d)", count_to_cell(next_point->X));
 		if (!(count_to_cell(next_point->X) == SHRT_MAX || count_to_cell(next_point->X) == SHRT_MIN)) {
 			delta_y = count_to_cell(target_point.Y) - map_get_y_cell();
 			if (delta_y != 0 && std::abs(delta_y <= 2)) {
 				next_point->Y = target_point.Y;
 				g_cm_move_type = ((dir == POS_X ^ delta_y > 0 ) ? CM_FOLLOW_LEFT_WALL : CM_FOLLOW_RIGHT_WALL);
-				ROS_ERROR("follow wall to new line, 2_left_3_right(%d)",g_cm_move_type);
+//				ROS_ERROR("don't need to go to new line. curr_x(%d)", count_to_cell(next_point->X));
+				ROS_INFO("follow wall to new line, 2_left_3_right(%d)",g_cm_move_type);
 			}
 		}
 	}
@@ -1080,7 +1081,10 @@ uint8_t cm_follow_wall(Point32_t target)
 		}
 
 		if(g_bumper_hitted || g_cliff_triggered){
-				cm_move_back();
+			cm_move_back();
+			g_bumper_hitted = false;
+			g_cliff_triggered = false;
+//			sleep(2);
 		}
 		if(get_clean_mode() == Clean_Mode_WallFollow){
 			if(wf_is_end()){
