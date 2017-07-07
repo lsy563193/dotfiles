0503740ad8699709cd9b128e77651f2841e511ca
lsy563193 <lsy563193>
2017-07-07 09:21:01 +0800

    refactor: move linearSpeedRegulator to regulator

--- include/regulator.h
+++ include/regulator.h
@@ -23,23 +23,6 @@ public:
 	static Point32_t s_origin;
 };
 
-class FollowWallRegulator:public RegulatorBase{
-
-public:
-	FollowWallRegulator(CMMoveType type);
-	~FollowWallRegulator(){ set_wheel_speed(0,0); };
-	bool adjustSpeed(int32_t &left_speed, int32_t &right_speed);
-	bool isReach();
-	bool isSwitch();
-//	void setTarget(Point32_t target){s_target = target;};
-//	void setOrigin(Point32_t origin){s_origin = origin;};
-
-private:
-	int32_t	 previous_;
-	CMMoveType type_;
-	int jam_;
-};
-
 class BackRegulator: public RegulatorBase{
 public:
 	BackRegulator();
@@ -76,6 +59,64 @@ private:
 	uint16_t speed_max_;
 };
 
+class TurnSpeedRegulator{
+public:
+	TurnSpeedRegulator(uint8_t speed_max,uint8_t speed_min, uint8_t speed_start):
+					speed_max_(speed_max),speed_min_(speed_min), speed_(speed_start),tick_(0){};
+	~TurnSpeedRegulator() {
+		set_wheel_speed(0, 0);
+	}
+	bool adjustSpeed(int16_t diff, uint8_t& speed_up);
+
+private:
+	uint8_t speed_max_;
+	uint8_t speed_min_;
+	uint8_t speed_;
+	uint32_t tick_;
+};
+
+class SelfCheckRegulator{
+public:
+	SelfCheckRegulator(){};
+	~SelfCheckRegulator(){
+		set_wheel_speed(0, 0);
+	};
+	bool adjustSpeed(uint8_t bumper_jam_state);
+};
+
+class FollowWallRegulator:public RegulatorBase{
+
+public:
+	FollowWallRegulator(CMMoveType type);
+	~FollowWallRegulator(){ set_wheel_speed(0,0); };
+	bool adjustSpeed(int32_t &left_speed, int32_t &right_speed);
+	bool isReach();
+	bool isSwitch();
+//	void setTarget(Point32_t target){s_target = target;};
+//	void setOrigin(Point32_t origin){s_origin = origin;};
+
+private:
+	int32_t	 previous_;
+	CMMoveType type_;
+	int jam_;
+};
+
+class LinearSpeedRegulator{
+public:
+	LinearSpeedRegulator(int32_t max):speed_max_(max),integrated_(0),base_speed_(BASE_SPEED),integration_cycle_(0),tick_(0),turn_speed_(4){};
+	~LinearSpeedRegulator(){
+		set_wheel_speed(0, 0);
+	};
+	bool adjustSpeed(Point32_t Target, bool slow_down, bool &rotate_is_needed);
+
+private:
+	int32_t speed_max_;
+	int32_t integrated_;
+	int32_t base_speed_;
+	uint8_t integration_cycle_;
+	uint32_t tick_;
+	uint8_t turn_speed_;
+};
 
 class RegulatorProxy:public RegulatorBase{
 public:
