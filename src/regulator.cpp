98916e06a9273242a7e58e89a67e8d4d356e619b
alvin <1937321986@qq.com>
2017-09-08 12:19:36 +0800

    fix(g_straight_distance): Increase the g_straight_distance.

--- src/regulator.cpp
+++ src/regulator.cpp
@@ -69,11 +69,11 @@ static int16_t bumper_turn_angle()
 		}
 		ROS_WARN("%s, %d: g_turn_angle(%d)",__FUNCTION__,__LINE__, g_turn_angle);
 
-		g_straight_distance = 100; //250;
+		g_straight_distance = 250; //250;
 		jam = get_wheel_step() < 2000 ? ++jam : 0;
 	}
 	ROS_INFO("strength in bumper_turn_angular: %d",strength);
-	g_straight_distance = 100;
+	g_straight_distance = 200;
 	reset_wheel_step();
 	if(mt_is_right())
 		g_turn_angle = -g_turn_angle;
