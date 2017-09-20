f8ef1853c918e87476cc3ba61ff744b1b45c4403
alvin <1937321986@qq.com>
2017-09-20 11:03:08 +0800

    fix(laserMarker): Fix the problem that laserMarker cant be used in Exploration mode.

--- src/laser.cpp
+++ src/laser.cpp
@@ -834,7 +834,7 @@ uint8_t Laser::laserMarker(bool is_mark,double X_MIN,double X_MAX)
 		is_triggered |= setLaserMarkerAcr2Dir(X_MIN, X_MAX, 168, 191, 2, 0, &laserScanData_, &laser_status,
 																				 Status_Front_OBS);
 	}
-	if(get_clean_mode() == Clean_Mode_Navigation)
+	if(get_clean_mode() == Clean_Mode_Navigation || get_clean_mode() == Clean_Mode_Exploration)
 	{
 		//left middle
 		setLaserMarkerAcr2Dir(X_MIN, X_MAX, 258, 281, 0, 2, &laserScanData_, &laser_status, 0);
