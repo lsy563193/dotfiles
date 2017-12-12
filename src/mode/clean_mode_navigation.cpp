be7ac241e4adce8fbf1a0948b53181f34e3a069f
lsy563193 <lsy563193>
2017-12-12 15:25:18 +0800

    refactor{clean_mode_navigation}: CleanModeNav fix use_unknown

--- src/mode/clean_mode_navigation.cpp
+++ src/mode/clean_mode_navigation.cpp
@@ -413,8 +413,10 @@ TargetList CleanModeNav::getReachableTargets(GridMap &map, const Cell_t &curr_ce
 {
 	map.generateSPMAP(curr_cell, possible_targets);
 	TargetList reachable_targets{};
+	ROS_WARN("~%s %d", __FUNCTION__, __LINE__);
 	for (auto it = possible_targets.begin(); it != possible_targets.end();)
 	{
+		ROS_WARN("~%s %d", __FUNCTION__, __LINE__);
 		CellState it_cost = map.getCell(COST_MAP, it->X, it->Y);
 		if (it_cost == COST_NO || it_cost == COST_HIGH)
 			continue;
@@ -424,6 +426,7 @@ TargetList CleanModeNav::getReachableTargets(GridMap &map, const Cell_t &curr_ce
 			it++;
 		}
 	}
+	ROS_WARN("~%s %d", __FUNCTION__, __LINE__);
 	return reachable_targets;
 }
 
