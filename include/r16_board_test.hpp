531a6f78fc030a0455dc2c6edf7bc85b78795c2c
AustinXDLiu <scutliuxudong@gmail.com>
2018-01-26 14:18:06 +0800

    feat(r16): Add files for r16 board test.(Not completed)

---
+++ include/r16_board_test.hpp
@@ -0,0 +1,21 @@
+//
+// Created by austin on 18-1-26.
+//
+
+#ifndef PP_R16_BOARD_TEST_HPP
+#define PP_R16_BOARD_TEST_HPP
+
+#endif //PP_R16_BOARD_TEST_HPP
+
+#include "config.h"
+
+#if R16_BOARD_TEST
+/*
+ * Board test for r16.(Not completed)
+ *
+ * Current containing test items:
+ *
+ *
+ */
+void r16_board_test();
+#endif
---
+++ src/r16_board_test.cpp
@@ -0,0 +1,10 @@
+//
+// Created by austin on 18-1-26.
+//
+
+#if R16_BOARD_TEST
+void r16_board_test()
+{
+
+}
+#endif
