c34b65227a7f1fa1d10d87b94158ca042de481e6
mengshige <mengshige1988@qq.com>
2018-03-02 18:36:46 +0800

    fix (script):fix some bug in mountusb.sh & unmountusb.sh

--- script/mountusb.sh
+++ script/mountusb.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-mount_tate=0
+mount_state=0
 cp_state1=0
 cp_state2=0
 kill_pp_state=0
@@ -8,20 +8,25 @@ pp_log_size=0
 pp_log1="/tmp/.ros/log/latest"
 pp_log2="/tmp/pp.log"
 dir_usb="/mnt/usb"
+wav_play="/opt/ros/indigo/lib/wav_play/wav_play"
+wav_dir="/opt/ros/indigo/share/pp/audio"
 
 #### mount device #####
 if [ ! -d $dir_usb ];then
 	mkdir $dir_usb && chmod 755 $dir_usb
 fi
 
-mount -t vfat /dev/$1 /mnt/usb
-if [ "$?" -eq 0 ];then
-	mount_state=1
-	echo "mount /dev/$1 on /mnt/usb success!!"
-else
-	/opt/ros/indigo/lib/wav_play/wav_play /opt/ros/indigo/share/pp/audio/53.wav
+if [ $# -ne 0 ];then
+	mount -t vfat /dev/$1 /mnt/usb
+	if [ "$?" -eq 0 ];then
+		mount_state=1
+		echo "mount /dev/$1 on /mnt/usb success!!"
+	else
+		echo "mount fail!"
+		#$wav_play "$wav_dir/53.wav"
+	fi
+	sync
 fi
-sync
 
 #### kill pp ####
 
@@ -30,45 +35,53 @@ proc_id=0
 proc_id=`pgrep -lf roslaunch | awk '{print $1}'`
 echo "pp process id = $proc_id"
 
-while true
+while [ -n "$proc_id" ]
 do
 	echo "kill pp..."
 	kill $proc_id
-	pgrep -lf "roslaunch"
-	if [ $? -eq 1 ];then
+	sleep 1
+	proc_id=`pgrep -lf roslaunch | awk '{print $1}'`
+	if [ -z $proc_id ];then
 		kill_pp_state=1
-		echo "kill pp id= $proc_id ,state=$kill_pp_state"
 		echo "kill pp state=$kill_pp_state"
 		break
 	fi
 done
 
 #### copy log files ####
-if [ "$mount_state" -eq 1 -a $kill_pp_state -eq 1 ];then
-	/opt/ros/indigo/lib/wav_play/wav_play /opt/ros/indigo/share/pp/audio/51.wav        
+if [ $mount_state -eq 1 ];then
+	$wav_play "$wav_dir/51.wav"
 	cp -rLP $pp_log1 /mnt/usb
 	if [ "$?" -eq 0 ];then
 		echo "copy $pp_log2 success!"
 		cp_state1=1
+	else
+		echo "copy $pp_log2 fail!"
 	fi
+
 	cp $pp_log2 /mnt/usb
 	if [ "$?" -eq 0 ];then                   
 		echo "copy $pp_log1 success!"
 		cp_state2=1
-	fi
-	sync													 
-	if [ "$cp_state1" -eq 1 -a "$cp_state2" -eq 1 ];then                                                      
-		/opt/ros/indigo/lib/wav_play/wav_play /opt/ros/indigo/share/pp/audio/52.wav
-	else                                                                               
-		/opt/ros/indigo/lib/wav_play/wav_play /opt/ros/indigo/share/pp/audio/53.wav
+	else
+		echo "copy $pp_log1 fail!"
 	fi
 
-	#### unmount device ####
+	sync													 
+	if [ "$cp_state1" -eq 1 -a "$cp_state2" -eq 1 ];then                                                      
+		$wav_play "$wav_dir/52.wav"
+	else                                                                               
+		$wav_play "$wav_dir/53.wav"      
+	fi
+fi
+
+#### unmount device ####
+if [ $mount_state -eq 1 ];then
 	umount /mnt/usb  
 	echo "umount usb device"
 fi
 
-#### clear log2 file ####
+#### clear log file ####
 pp_log_size=`ls -l $pp_log2 | awk ' {print $5}'`
 echo "$pp_log2 size = $pp_log_size"
 if [ $pp_log_size -gt 1024000 ];then
--- script/unmountusb.sh
+++ script/unmountusb.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
-if [ -e /dev/sda ];then
+start_pp=/opt/ros/indigo/bin/start_pp.sh
+ls /de/sd*
+if [ $? -eq 0 ];then
 	umount /mnt/usb
 	sync
 	/opt/ros/indigo/lib/wav_play/wav_play /opt/ros/indigo/share/pp/audio/32.wav
 fi
-exit 0
 
+#pid=`pgrep -lf roslaunch | awk '{print $1}'`
+#if [ -z $pid ];then
+#	$start_pp
+#fi	
+exit 0
