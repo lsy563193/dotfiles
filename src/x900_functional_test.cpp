b12a83572f0ea876a968eec19d75c9f8a977d9fc
lsy <526154809@qq.com>
2018-04-04 13:34:27 +0800

    fix(functional test): fix error in main brush current test.

--- src/x900_functional_test.cpp
+++ src/x900_functional_test.cpp
@@ -309,6 +309,7 @@ void main_board_test(uint8_t &test_stage, uint16_t &error_code, uint16_t &curren
 				break;
 			case FUNC_MAINBRUSH_TEST_MODE:/*--- main brush ---*/
 				main_brush_test(baseline, test_stage, error_code, current_data);
+				break;
 			case FUNC_CHARGE_CURRENT_TEST_MODE:/*---charge current---*/
 				charge_current_test(is_fixture, test_stage, error_code, current_data);
 				break;
@@ -1037,7 +1038,7 @@ void wheels_test(uint16_t *baseline, uint8_t &test_stage, uint16_t &error_code, 
 				break;
 			case 3:
 				step++;
-				if (current_current < 30 || current_current > 100 || motor_current < 20 || motor_current > 100) {
+				if (current_current < 30 || current_current > 150 || motor_current < 20 || motor_current > 100) {
 					error_code = LEFT_WHEEL_FORWARD_CURRENT_ERROR;
 					current_data = static_cast<uint16_t>(motor_current);
 					return ;
@@ -1099,7 +1100,7 @@ void wheels_test(uint16_t *baseline, uint8_t &test_stage, uint16_t &error_code, 
 				break;
 			case 8:
 				step++;
-				if (current_current < 30 || current_current > 100 || motor_current < 20 || motor_current > 100) {
+				if (current_current < 30 || current_current > 150 || motor_current < 20 || motor_current > 100) {
 					error_code = LEFT_WHEEL_BACKWARD_CURRENT_ERROR;
 					current_data = static_cast<uint16_t>(motor_current);
 					return ;
@@ -1211,7 +1212,7 @@ void wheels_test(uint16_t *baseline, uint8_t &test_stage, uint16_t &error_code, 
 				break;
 			case 16:
 				step++;
-				if (current_current < 30 || current_current > 100 || motor_current < 20 || motor_current > 100) {
+				if (current_current < 30 || current_current > 150 || motor_current < 20 || motor_current > 100) {
 					error_code = RIGHT_WHEEL_FORWARD_CURRENT_ERROR;
 					current_data = motor_current;
 					return ;
@@ -1273,7 +1274,7 @@ void wheels_test(uint16_t *baseline, uint8_t &test_stage, uint16_t &error_code, 
 				break;
 			case 21:
 				step++;
-				if (current_current < 30 || current_current > 100 || motor_current < 20 || motor_current > 100) {
+				if (current_current < 30 || current_current > 150 || motor_current < 20 || motor_current > 100) {
 					error_code = RIGHT_WHEEL_BACKWARD_CURRENT_ERROR;
 					current_data = motor_current;
 					return ;
@@ -1696,7 +1697,7 @@ void main_brush_test(uint16_t *baseline, uint8_t &test_stage, uint16_t &error_co
 				break;
 			case 3:
 				step++;
-				if (current_current < 100 || current_current > 300 || motor_current < 100 || motor_current > 300) {
+				if (current_current < 130 || current_current > 350 || motor_current < 100 || motor_current > 300) {
 					error_code = MAIN_BRUSH_CURRENT_ERROR;
 					current_data = static_cast<uint16_t>(motor_current);
 					return ;
