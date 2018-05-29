2f92e1c26406c1202d163b3788130ff061c75a90
AustinXDLiu <scutliuxudong@gmail.com>
2018-05-29 17:37:01 +0800

    Update for r15.

--- include/config.h
+++ include/config.h
@@ -3,7 +3,7 @@
 /* ------------------------------------- System Setting ------------------------------------- */
 
 #define CURRENT_VERSION 0
-#define CURRENT_PATCH 14
+#define CURRENT_PATCH 15
 
 /*
  * Chipset setting.
