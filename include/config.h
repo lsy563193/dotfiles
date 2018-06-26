9fb3ea90ab650808db9aa5bc687d1eb8d6c0b5e0
AustinXDLiu <scutliuxudong@gmail.com>
2018-06-26 11:22:23 +0800

    Update for r18.

--- include/config.h
+++ include/config.h
@@ -3,7 +3,7 @@
 /* ------------------------------------- System Setting ------------------------------------- */
 
 #define CURRENT_VERSION 0
-#define CURRENT_PATCH 17
+#define CURRENT_PATCH 18
 
 /*
  * Chipset setting.
