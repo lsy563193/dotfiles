045bae9c38b216d834e115dd043d1e7e4eb068df
AustinXDLiu <scutliuxudong@gmail.com>
2018-06-27 13:56:06 +0800

    Update for r19.

--- include/config.h
+++ include/config.h
@@ -3,7 +3,7 @@
 /* ------------------------------------- System Setting ------------------------------------- */
 
 #define CURRENT_VERSION 0
-#define CURRENT_PATCH 18
+#define CURRENT_PATCH 19
 
 /*
  * Chipset setting.
