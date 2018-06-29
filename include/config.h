15ba5f7edf03d623bda8c3e31bffed57f141725f
AustinXDLiu <scutliuxudong@gmail.com>
2018-06-29 09:30:02 +0800

    Update for r20.

--- include/config.h
+++ include/config.h
@@ -3,7 +3,7 @@
 /* ------------------------------------- System Setting ------------------------------------- */
 
 #define CURRENT_VERSION 0
-#define CURRENT_PATCH 19
+#define CURRENT_PATCH 20
 
 /*
  * Chipset setting.
